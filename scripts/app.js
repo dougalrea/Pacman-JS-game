6f739c64cf2e30fcf576e1d6fd5417e36e99db2f
Dougal Rea <dougal.rea@gmail.com>
2020-11-20 15:43:21 +0000

    pacman movement with rotation sorted. also wall functionality and map borders added

--- scripts/app.js
+++ scripts/app.js
@@ -28,47 +28,63 @@ function init () {
   //! Pacman Inital
   // Variables
 
-  let pacmanPosition = Math.round((cellCount * 179) / 256) // NOT TO BE CONFUSED WITH PACMANCELL
+  let pacmanPosition = Math.round((cellCount * 179) / 256) + 75 // NOT TO BE CONFUSED WITH PACMANCELL
+  let currentRotation = 180
   let pacmanDirection = undefined
 
   // Functions
 
-  function addPacmanToCell (position) {
+  function addPacmanToCell (position, rotation) {
     cells[position].classList.remove('freshCell')
-    cells[position].classList.add('pacman')
+    cells[position].classList.add(`pacman${rotation}`)
   }
 
   // Invocations
 
-  addPacmanToCell(pacmanPosition)
+  addPacmanToCell(pacmanPosition, currentRotation)
 
   //! Pacman Behaviour
 
   // Variables
   let movementTimer = undefined
-  let currentRotation = 0
+  let pendingRotation = 0
   const pacmanCell = document.querySelector('div.pacman')
 
   // Functions
 
-  function removePacman (position) {
-    cells[position].classList.remove('pacman')
+  function removePacman (position, rotation) {
+    cells[position].classList.remove(`pacman${rotation}`)
   }
 
-  function rotatePacman (degrees, directionOfTravel) {
-    pacmanCell.style.transform = `rotate(${degrees}deg)`
-    pacmanDirection = directionOfTravel
-    console.log(
-      'RotatePacman Function says',
-      pacmanCell.style.transform,
-      pacmanDirection
-    )
-  }
-  
+  // function rotatePacman (direction) {
+  //   switch (direction) {
+  //     case 'up':
+  //       removePacman(pacmanPosition, currentRotation)
+  //       currentRotation = 90
+  //       addPacmanToCell(pacmanPosition, currentRotation)
+  //       break
+  //     case 'right':
+  //       removePacman(pacmanPosition, currentRotation)
+  //       currentRotation = 180
+  //       addPacmanToCell(pacmanPosition, currentRotation)
+  //       break
+  //     case 'down':
+  //       removePacman(pacmanPosition, currentRotation)
+  //       currentRotation = 270
+  //       addPacmanToCell(pacmanPosition, currentRotation)
+  //       break
+  //     case 'left':
+  //       removePacman(pacmanPosition, currentRotation)
+  //       currentRotation = 0
+  //       addPacmanToCell(pacmanPosition, currentRotation)
+  //   }
+  // }
+
   function assignPacmanRotationAndDirection (event) {
     if (movementTimer) {
       clearInterval(movementTimer)
     }
+    removePacman(pacmanPosition, currentRotation)
     switch (event.key) {
       case 'ArrowUp':
         currentRotation = 90
@@ -89,36 +105,118 @@ function init () {
       default:
         console.log('INVALID KEY')
     }
-    movePacman(pacmanDirection)
-    movementTimer = setInterval(movePacman, 155, pacmanDirection)
+    movePacman(pacmanDirection, currentRotation)
+    movementTimer = setInterval(movePacman, 800, pacmanDirection)
+  }
+
+  function stopRightThereMister () {
+    clearInterval(movementTimer)
+    addPacmanToCell(pacmanPosition, currentRotation)
   }
 
   function movePacman (direction) {
     if (!direction) {
       return
     }
-    removePacman(pacmanPosition)
+
+    let adjacentCellUp = undefined
+    let adjacentCellRight = undefined
+    let adjacentCellDown = undefined
+    let adjacentCellLeft = undefined
+
+    removePacman(pacmanPosition, currentRotation)
+
     switch (direction) {
       case 'up':
-        addPacmanToCell((pacmanPosition -= 25))
+        adjacentCellUp = pacmanPosition - width
+        if (cells[adjacentCellUp].classList.contains('wall')) {
+          stopRightThereMister()
+        } else addPacmanToCell((pacmanPosition -= width), 90)
         break
       case 'right':
-        addPacmanToCell((pacmanPosition += 1))
+        adjacentCellRight = pacmanPosition + 1
+        if (cells[adjacentCellRight].classList.contains('wall')) {
+          stopRightThereMister()
+        } else addPacmanToCell((pacmanPosition += 1), 180)
         break
       case 'down':
-        addPacmanToCell((pacmanPosition += 25))
+        adjacentCellDown = pacmanPosition + width
+        if (cells[adjacentCellDown].classList.contains('wall')) {
+          stopRightThereMister()
+        } else addPacmanToCell((pacmanPosition += width), 270)
         break
       case 'left':
-        addPacmanToCell((pacmanPosition -= 1))
+        adjacentCellLeft = pacmanPosition - 1
+        if (cells[adjacentCellLeft].classList.contains('wall')) {
+          stopRightThereMister()
+        } else addPacmanToCell((pacmanPosition -= 1), 0)
         break
       default:
         console.log('eek')
     }
-    rotatePacman(currentRotation, pacmanDirection)
-    console.log('timer works')
+    console.log(
+      'cell to the right is',
+      adjacentCellRight,
+      'current position is',
+      pacmanPosition
+    )
   }
 
   //Event Listeners
   document.addEventListener('keydown', assignPacmanRotationAndDirection)
+
+  //! Map Generation
+
+  // Variables
+  const wallCells = []
+
+  function generateMapOne () {
+    for (let i = 0; i < width; i++) {
+      // TOP WALL
+      wallCells.push(cells[i])
+    }
+    for (let i = 600; i < width ** 2; i++) {
+      // BOTTOM WALL
+      wallCells.push(cells[i])
+    }
+    for (let i = 24; i < 225; i += 25) {
+      // RIGHT WALL (TOP)
+      wallCells.push(cells[i])
+    }
+    for (let i = 424; i < width ** 2; i += 25) {
+      // ROGHT WALL (BOTTOM)
+      wallCells.push(cells[i])
+    }
+    for (let i = 25; i < 200; i += 25) {
+      // LEFT WALL (TOP)
+      wallCells.push(cells[i])
+    }
+    for (let i = 400; i < 600; i += 25) {
+      // LEFT WALL (BOTTOM)
+      wallCells.push(cells[i])
+    }
+
+    // LEFT WALL INDENTS
+    for (let i = 400; i < 406; i++) {
+      wallCells.push(cells[i])
+    }
+    for (let i = 200; i < 206; i++) {
+      wallCells.push(cells[i])
+    }
+
+    // RIGHT WALL INDENTS
+    for (let i = 224; i > 218; i--) {
+      wallCells.push(cells[i])
+    }
+    for (let i = 424; i > 418; i--) {
+      wallCells.push(cells[i])
+    }
+
+    wallCells.forEach(cell => {
+      cell.classList.remove('freshCell')
+      cell.classList.add('wall')
+    })
+  }
+  generateMapOne()
 }
 window.addEventListener('DOMContentLoaded', init)
